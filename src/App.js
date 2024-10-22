65dbb9639dd7fb9bc01679ed81782b979834515e
aldyneronqs <aldyneronquillo91403@gmail.com>
2024-10-22 11:20:45 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+function Display({ display }) {
+  return (
+    <div className="Display">
+      {display}
+    </div>
+  );
+}
+
+function Key({ label, clickHandler, className = "" }) {
+  return (
+    <button className={`ButtonKeys ${className}`} onClick={clickHandler}>
+      {label}
+    </button>
+  );
+}
 
 function App() {
+  const [disp, setDisp] = useState("10 Things That Require Zero Talent");
+
+  const items = [
+    "Being On Time",               // 1
+    "Making An Effort",            // 2
+    "Being High Energy",           // 3
+    "Having A Positive Attitude",  // 4
+    "Being Passionate",            // 5
+    "Using Good Body Language",     // 6
+    "Being Coachable",             // 7
+    "Doing A Little Extra",        // 8
+    "Being Prepared",              // 9
+    "Having A Strong Work Ethic"   // 0
+  ];
+
+  const itemClickHandler = (index) => {
+    setDisp(items[index]);
+  };
+
+  const resetClickHandler = () => {
+    setDisp("10 Things That Require Zero Talent");
+  };
+
+  const nameClickHandler = () => {
+    setDisp('ALDYNE RONQUILLO'); 
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ALDYNE RONQUILLO - IT3A</h1>
+
+      <div className="CalcContainer">
+        <div className="DispContainer">
+          <Display display={disp} />
+        </div>
+
+        <div className="ButtonsContainer">
+          {items.map((item, index) => (
+            <Key key={index} label={index === 9 ? '0' : index + 1} clickHandler={() => itemClickHandler(index)} />
+          ))}
+          <Key label={'RESET'} clickHandler={resetClickHandler} className="ClearButton" />
+          <Key label={'NAME'} clickHandler={nameClickHandler} className="OperatorButton" />
+        </div>
+      </div>
     </div>
   );
 }
